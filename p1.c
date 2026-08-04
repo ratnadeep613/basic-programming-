17b979a9797282a6771d00c3c7c39d4379ebdb56
ratnadeep613 <ratnadeeppatil7173@gmail.com>
2026-08-04 11:59:32 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+
+	int arr[5]={5,4,3,2,1};
+	
+	
+	  int key=1;
+	  int i,n=5;
+	  
+	   for(i=0;i<n;i++)
+
+
+  {
+    if(arr[i]==key)
+  printf("%d found at index",key,i);
+}
+    for(i=0;i>n;i--)
+   {
+	printf("element not found");
+	return 0;
+   }
+};
---
+++ p2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+   void main()
+   
+   {
+   	  int a,b,c;
+   	  
+   	  
+   	  printf("Enter Any Number");
+   	  scanf("%d%d%d",&a,&b,&c);
+   }
